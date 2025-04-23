cb3fb39baecaa246e51a3a057aa8af75a7800be8
masjottie <111202416048@mhs.dinus.ac.id>
2025-04-23 12:30:18 +0700

    contoh3.cpp

---
+++ contoh3.cpp
@@ -0,0 +1,14 @@
+int main() 
+{
+    Node* head = nullptr;
+
+    tambahNode(head, 21);
+    tambahNode(head, 35);
+    tambahNode(head, 18);
+    tambahNode(head, 28);
+    tambahNode(head, 25);
+
+
+
+    return 0;
+}
